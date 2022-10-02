a959a023c2726b5edd22310fd2929712f07e0c8f
voquylong <long@idctechno.com>
2022-10-02 22:14:55 +0700

    Create table migrations

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,75 @@
+<?php
+
+use app\core\Application;
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+class m0001_initial
+{
+    public function up()
+    {
+        $db = Application::$app->db;
+        $sql = "
+            --
+            -- Table structure for table `users`
+            --
+
+            CREATE TABLE `users` (
+            `id` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `firstname` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `lastname` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `email` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `phone_number` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `password` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `image_url` varchar(4000) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `address` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `ward_id` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `district_id` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `province_id` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `role` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+            `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+            -- ---------------------------------------------------------
+            --
+            -- Table structure for table `stores`
+            --
+
+            CREATE TABLE `stores` (
+            `id` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `address` varchar(1000) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `status` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `image_url` varchar(4000) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `open_time` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `phone` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+            `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+            -- ---------------------------------------------------------
+            --
+            -- Indexes for table `users`
+            --
+            ALTER TABLE `users`
+            ADD PRIMARY KEY (`id`);
+
+            --
+            -- Indexes for table `stores`
+            --
+            ALTER TABLE `stores`
+            ADD PRIMARY KEY (`id`);
+        ";
+        $db->pdo->exec($sql);
+    }
+
+    public function down()
+    {
+        $db = Application::$app->db;
+        $resolution = explode('dbname=', $_ENV['DB_DSN']);
+		$dbname = $resolution[1];
+        $sql = "DROP DATABASE $dbname;";
+        $db->pdo->exec($sql);
+    }
+}
