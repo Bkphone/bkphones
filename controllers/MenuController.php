5304e47b12f10b35976a55e936382fcd9cbc6ebb
voquylong <long@idctechno.com>
2022-10-17 23:12:27 +0700

    Update controllers

---
+++ controllers/MenuController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+use app\models\Product;
+use app\middlewares\AuthMiddleware;
+
+class MenuController extends Controller
+{
+    public function __construct()
+    {
+        $this->registerMiddleware(new AuthMiddleware(['menu']));
+    }
+
+    public function home()
+    {
+        $samsungs = Product::getSpecialProduct(8, 2, 4);
+        $iphones = Product::getSpecialProduct(8, 1, 4);
+        $watchs = Product::getSpecialProduct(9, null, 4);
+
+        return $this->render('home', [
+            'samsungs' => $samsungs,
+            'iphones' => $iphones,
+            'watchs' =>  $watchs
+        ]);
+    }
+
+    public function menu()
+    {
+        $products = Product::getAllProducts();
+        return $this->render('menu', [
+            'products' => $products
+        ]);
+    }
+}
