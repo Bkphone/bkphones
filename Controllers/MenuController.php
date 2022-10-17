920e56cece7012e1fdb84cb8f07493a8e583ca17
voquylong <long@idctechno.com>
2022-10-17 09:42:15 +0700

    Update quota for special products

--- Controllers/MenuController.php
+++ Controllers/MenuController.php
@@ -15,9 +15,9 @@ class MenuController extends Controller
 
     public function home()
     {
-        $samsungs = Product::getSpecialProduct(9, 2);
-        $iphones = Product::getSpecialProduct(8, 1);
-        $watchs = Product::getSpecialProduct(9);
+        $samsungs = Product::getSpecialProduct(9, 2, 4);
+        $iphones = Product::getSpecialProduct(8, 1, 4);
+        $watchs = Product::getSpecialProduct(9, null, 4);
 
         return $this->render('home', [
             'samsungs' => $samsungs,
