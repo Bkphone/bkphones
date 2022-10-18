e98809ab77ebe75411f8c864394494bff88366f7
voquylong <long@idctechno.com>
2022-10-18 11:20:20 +0700

    Update docker-composer and auto install

--- auto_install.sh
+++ auto_install.sh
@@ -1,3 +1,6 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
+docker-compose exec app composer install
+docker-compose exec app cp .env.example .env
+docker-compose exec app php migrations.php
