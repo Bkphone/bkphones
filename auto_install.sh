07810144d26ef809465b599a13c03ce6c76d1fe2
voquylong <long@idctechno.com>
2022-10-18 15:03:27 +0700

    Update auto build and deploy

--- auto_install.sh
+++ auto_install.sh
@@ -3,4 +3,5 @@ docker-compose build
 docker-compose up -d
 docker-compose exec app composer install
 docker-compose exec app cp .env.example .env
+docker exec mysql bash /script/init.sh
 docker-compose exec app php migrations.php
