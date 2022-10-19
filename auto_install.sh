9ba18cf4bbc2d94e4b0702fd622ad97963b7e550
voquylong <long@idctechno.com>
2022-10-19 14:04:03 +0700

    Update auto_install

--- auto_install.sh
+++ auto_install.sh
@@ -5,5 +5,5 @@ docker-compose exec app composer install
 docker-compose exec app cp .env.example .env
 docker cp config.cnf mysql:/config.cnf
 docker cp init.sh mysql:/init.sh
-docker exec mysql bash /init.sh
+docker exec -t mysql bash /init.sh
 docker-compose exec app php migrations.php
