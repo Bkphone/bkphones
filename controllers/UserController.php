71a0255c8f8ed5c4a5b8940ffe7b7087df6de391
dat <tandatsm@gmail.com>
2022-12-05 20:49:58 +0700

    user controller

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -8,6 +8,15 @@ use app\core\Controller;
 use app\core\Application;
 use app\middlewares\AdminMiddleWare;
 use app\models\User;
+use app\core\Request;
+use app\exception\ForbiddenException;
+use app\models\LoginForm;
+
+use app\middlewares\AuthMiddleWare;
+
+use app\models\Store;
+use app\core\Form\Field;
+use app\core\Model;
 
 class UserController extends Controller{
     public function __construct() {
@@ -22,4 +31,66 @@ class UserController extends Controller{
             'users' => $users
         ]);
     }
-}
+    public function create(Request $request)
+    {
+          $usermodel = new User();
+       // $usermodel = User::getUserInfo(Application::$app->user->id);
+        if ($request->getMethod() === 'post') {
+            $usermodel->loadData($request->getBody());
+            $role = $usermodel->role;
+            if ($usermodel->validate() && $usermodel->saveAdmin($role)) {
+                Application::$app->session->setFlash('success', 'You have added a new user');
+                Application::$app->response->redirect('create');
+                return 'Show success page';
+            }
+        }
+        $this->setLayout('admin');
+        return $this->render('create', [
+            'model' => $usermodel
+        ]);
+    }
+   
+    public function update(Request $request)
+    {     $id = $_GET["id"];
+          $usermodel = User::getUserInfo($id);
+        if ($request->getMethod() === 'post') {
+            $usermodel->loadData($request->getBody());
+            if ($usermodel->updateProfile($usermodel)) {
+                Application::$app->session->setFlash('success', 'You have edited success');
+                Application::$app->response->redirect('/admin/users');
+                return 'Show success page';
+            }
+        }
+        $this->setLayout('admin');
+        return $this->render('edit', [
+            'model' => $usermodel
+        ]);
+    }
+    public function password(Request $request)
+    {     $id = $_GET["id"];
+          $usermodel = User::getUserInfo($id);
+        if ($request->getMethod() === 'post') {
+            $usermodel->loadData($request->getBody());
+            if ($usermodel->update($usermodel)) {
+                Application::$app->session->setFlash('success', 'You have edited success');
+                Application::$app->response->redirect('/admin/users');
+                return 'Show success page';
+            }
+        }
+        $this->setLayout('admin');
+        return $this->render('password', [
+            'model' => $usermodel
+        ]);
+    }
+    
+    public function delete()
+    {     $id = $_GET["id"];
+          $usermodel = User::getUserInfo($id);
+          $usermodel->delete();
+          Application::$app->session->setFlash('success', 'You have edited success');
+          Application::$app->response->redirect('/admin/users');  
+       
+     }
+       
+  
+    }
