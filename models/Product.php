eebe9dea5b766b7b63b7227d4e142094ba47769b
HiepNgo1609 <hiep.ngo1609@hcmut.edu.vn>
2022-12-05 20:28:48 +0700

    CRUD San pham

--- models/Product.php
+++ models/Product.php
@@ -13,6 +13,7 @@ class Product extends DBModel
     public string $name;
     public float $price_show;
     public float $price_through;
+    public float $discount;
     public string $description;
     public string $image_url;
 
@@ -24,7 +25,7 @@ class Product extends DBModel
         $price_through = 0,
         $discount = 0,
         $description = '',
-        $image_url = ''
+        $image_url = '',
     ) {
         $this->id = $id;
         $this->category_id = $category_id;
@@ -36,30 +37,75 @@ class Product extends DBModel
         $this->image_url = $image_url;
     }
 
-    public function setId($id) { $this->id = $id; }
-    public function getId() { return $this->id; }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
 
-    public function setCategoryId($category_id) { $this->category_id = $category_id; }
-    public function getCategoryId() { return $this->category_id; }
+    public function setCategoryId($category_id)
+    {
+        $this->category_id = $category_id;
+    }
+    public function getCategoryId()
+    {
+        return $this->category_id;
+    }
 
-    public function setName($name) { $this->name = $name; }
-    public function getName() { return $this->name; }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
 
-    public function getPriceShow() { return $this->price_show; }
-    public function getPriceThrough() { return $this->price_through; }
+    public function getPriceShow()
+    {
+        return $this->price_show;
+    }
+    public function getPriceThrough()
+    {
+        return $this->price_through;
+    }
 
-    public function setDescription($description) { $this->description = $description; }
-    public function getDescription() { return $this->description; }
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+    public function setDiscount($discount)
+    {
+        $this->discount = $discount;
+    }
 
-    public function setImageUrl($image_url) { $this->image_url = $image_url; }
-    public function getImageUrl() { return $this->image_url; }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setImageUrl($image_url)
+    {
+        $this->image_url = $image_url;
+    }
+    public function getImageUrl()
+    {
+        return $this->image_url;
+    }
 
     public function getCategory()
     {
         $categoryModel = Category::get($this->category_id);
         return $categoryModel->getDisplayName();
     }
-    
+
     public function getDisplayInfo(): string
     {
         return $this->id . ' ' . $this->category_id . ' ' . $this->name . ' ' . $this->price_show . ' ' . $this->description;
@@ -74,19 +120,21 @@ class Product extends DBModel
     {
         return ['id', 'category_id', 'name', 'price_show', 'price_through', 'discount', 'description', 'image_url'];
     }
-   
+
     public function labels(): array
     {
         return [
             'id' => 'Mã sản phẩm',
             'name' => 'Tên sản phẩm',
             'price_show' => 'Giá',
+            'price_through' => 'Giá niêm yết',
+            'discount' => 'Giảm giá',
             'description' => 'Mô tả sản phẩm',
             'image_url' => 'Hình ảnh sản phẩm',
-            'category_id' => 'Mã mục'
+            'category_id' => 'Danh mục'
         ];
     }
-    
+
     public function getLabel($attribute)
     {
         return $this->labels()[$attribute];
@@ -95,9 +143,13 @@ class Product extends DBModel
     public function rules(): array
     {
         return [
-            'name' => [self::RULE_REQUIRED, [self::RULE_MAX, 'max' <= 50]],
+            'name' => [self::RULE_REQUIRED],
             'description' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' >= 20], [self::RULE_MAX, 'max' <= 100]],
             'price_show' => [self::RULE_REQUIRED],
+            'price_through' => [self::RULE_REQUIRED],
+            'description' => [self::RULE_REQUIRED],
+            'image_url' => [self::RULE_REQUIRED],
+            'category_id' => [self::RULE_REQUIRED]
         ];
     }
 
@@ -107,28 +159,57 @@ class Product extends DBModel
         return parent::save();
     }
 
+    public static function getProductByPaging($page_idx, $num_products_per_page)
+    {
+        $list = [];
+        $db = Database::getInstance();
+        $starting_limit_number = ($page_idx - 1) * $num_products_per_page;
+        $sql = "SELECT * FROM products LIMIT " . $starting_limit_number . ',' . $num_products_per_page;
+        $req = $db->query($sql);
+        foreach ($req->fetchAll() as $item) {
+            $list[] = new Product(
+                $item['id'],
+                $item['category_id'],
+                $item['name'],
+                $item['price_show'],
+                $item['price_through'],
+                $item['discount'],
+                $item['description'],
+                $item['image_url']
+            );
+        }
+        return $list;
+    }
+
     public static function getSpecialProduct($discount, $category_id = null, $quota = 10)
     {
         $list = [];
         $db = Database::getInstance();
-        if($category_id) {
+        if ($category_id) {
             $req = $db->query("SELECT * FROM products WHERE discount > $discount AND category_id = $category_id");
         } else {
             $req = $db->query("SELECT * FROM products WHERE discount > $discount AND category_id >= 3");
         }
 
         foreach ($req->fetchAll() as $item) {
-            $list[] = new Product($item['id'], $item['category_id'], $item['name'], 
-                            $item['price_show'], $item['price_through'], $item['discount'], 
-                            $item['description'], $item['image_url']);
+            $list[] = new Product(
+                $item['id'],
+                $item['category_id'],
+                $item['name'],
+                $item['price_show'],
+                $item['price_through'],
+                $item['discount'],
+                $item['description'],
+                $item['image_url']
+            );
         }
-        
+
         if (count($list) > $quota) {
             $result = array_slice($list, 0, $quota);
         } else {
             $result = $list;
         }
-        
+
         return $result;
     }
 
@@ -139,11 +220,91 @@ class Product extends DBModel
         $req = $db->query('SELECT * FROM products');
 
         foreach ($req->fetchAll() as $item) {
-            $list[] = new Product($item['id'], $item['category_id'], $item['name'], 
-                            $item['price_show'], $item['price_through'], $item['discount'], 
-                            $item['description'], $item['image_url']);
+            $list[] = new Product(
+                $item['id'],
+                $item['category_id'],
+                $item['name'],
+                $item['price_show'],
+                $item['price_through'],
+                $item['discount'],
+                $item['description'],
+                $item['image_url']
+            );
         }
 
         return $list;
+    }
+
+    public static function getProductById($id)
+    {
+        $db = Database::getInstance();
+        $req = $db->query("SELECT * FROM products WHERE id = '$id'");
+        $item = $req->fetchAll()[0];
+        $product = new Product(
+            $item['id'],
+            $item['category_id'],
+            $item['name'],
+            $item['price_show'],
+            $item['price_through'],
+            $item['discount'],
+            $item['description'],
+            $item['image_url']
+        );
+        return $product;
+    }
+
+    public static function addNewProduct()
+    {
+        if (self::save()) {
+            return true;
+        }
+    }
+
+    public function updateProduct(Product $product)
+    {
+        $sql = "UPDATE products 
+                SET 
+                    category_id = '$product->category_id',
+                    name = '$product->name',
+                    price_show = $product->price_show,
+                    price_through = $product->price_through,
+                    discount = '$product->discount',
+                    description = '$product->description',
+                    image_url = '$product->image_url'
+                WHERE id = '$product->id'";
+        $statement = self::prepare($sql);
+        $statement->execute();
+        return true;
+    }
+
+    public static function searchProductByName($name)
+    {
+        $list = [];
+        $db = Database::getInstance();
+        $sql = "SELECT * FROM products WHERE name LIKE '%$name%'";
+        $sql .= "OR category_id like '%" . $name . "%'";
+        $req = $db->query($sql);
+        foreach ($req->fetchAll() as $item) {
+            $list[] = new Product(
+                $item['id'],
+                $item['category_id'],
+                $item['name'],
+                $item['price_show'],
+                $item['price_through'],
+                $item['discount'],
+                $item['description'],
+                $item['image_url']
+            );
+        }
+        return $list;
+    }
+
+    public function deleteProduct()
+    {
+        $tablename = $this->tableName();
+        $sql = "DELETE FROM $tablename WHERE id=?";
+        $stmt = self::prepare($sql);
+        $stmt->execute([$this->id]);
+        return true;
     }
 }
