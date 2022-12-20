c24b1663873b5dac8ef9745c79e5718b25fdaddd
voquylong <long@idctechno.com>
2022-12-20 09:46:59 +0700

    Update profile feature

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -4,15 +4,43 @@
 */
 namespace app\controllers;
 
-use app\core\Controller;
 use app\core\Application;
-use app\middlewares\AdminMiddleWare;
+use app\core\Controller;
+use app\core\Request;
+use app\middlewares\AdminMiddleware;
+use app\models\User;
 
 class AdminController extends Controller{
     
+
+    public function __construct() 
+    {
+        Application::$app->controller->registerMiddleware(new AdminMiddleware(['index']));
+    }
+
     public function index() 
     {
        $this->setLayout('admin');
       return $this->render('adminhome');
+    }
+
+    public function profile(Request $request)
+    {
+        $adminId = Application::$app->user->id;
+        $adminModel = User::getUserInfo($adminId);
+        if($request->getMethod() === 'post') {
+            $adminModel->loadData($request->getBody());
+            if ($adminModel->validateUpdateProfile() && true) {
+                if ($adminModel->updateProfile($adminModel)) {
+                    Application::$app->response->redirect('/admin/profile');
+                    return 'Show success page';
+                }
+            }
+        }
+
+        $this->setLayout('admin');
+        return $this->render('profile', [
+            'user' => $adminModel
+        ]);
     }
 }
